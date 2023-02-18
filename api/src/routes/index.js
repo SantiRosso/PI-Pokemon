8905a3d0b7d921d2489de8f0c7b5e05229342017
SantiRosso <santiagorosso@live.com.ar>
2023-02-18 13:19:25 -0300

    new route (getPokemons and getPokemonsByName)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const getPokemons = requiere("./pokemons/pokemons.js");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/pokemons", getPokemons);
 
 module.exports = router;
---
+++ api/src/routes/pokemons/pokemons.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const router = Router();
+const { getPokemons, getPokemonsByName } = require("./controllers.js");
+
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+  try {
+    if (name) {
+      res.send(await getPokemonsByName);
+    } else {
+      res.send(await getPokemons);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
