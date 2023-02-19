2a0649a76e5a081198bac215fee3b52ab5e5e52d
SantiRosso <santiagorosso@live.com.ar>
2023-02-19 16:18:31 -0300

    new route (getTypes)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const getPokemons = require("./pokemons/pokemons.js");
+const getTypes = requiere("./types/types.js");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/pokemons", getPokemons);
-
+router.use("/types", getTypes);
 module.exports = router;
---
+++ api/src/routes/types/types.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const router = Router();
+const {} = require("./controllers.js");
+
+router.get("/", async (req, res) => {
+  try {
+    res.send(await getTypes());
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
