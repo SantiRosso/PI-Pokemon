c1793d869d80ee68f65c6e20da599eefa891230d
SantiRosso <santiagorosso@live.com.ar>
2023-02-20 19:33:37 -0300

    enrutado

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
+import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+//components
+import Home from "./components/Home/Home";
+
 function App() {
   return (
-    <Routes>
-      {/* <Route exact path="/" element={<LoginRegister />} /> */}
-      {/* <Route path="/home" element={<Home />} /> */}
-      {/* <Route path="/detail/:id" element={<Detail />} /> */}
-      {/* <Route path="/profile" element={<Profile />} /> */}
-      {/* <Route path="/foro" element={<Foro />} /> */}
-      {/* <Route path="/profile" element={<Profile />} /> */}
-      {/* <Route path="/about" element={<About />} /> */}
-      {/* <Route path="/createPokemon" element={<CreatePokemon />} /> */}
-      {/* <Route path="/editPokemon" element={<EditPokemon />} /> */}
-    </Routes>
+    <div>
+      <Routes>
+        {/* <Route exact path="/" element={<LoginRegister />} /> */}
+        <Route path="/home" element={<Home />} />
+        {/* <Route path="/detail/:id" element={<Detail />} /> */}
+        {/* <Route path="/profile" element={<Profile />} /> */}
+        {/* <Route path="/foro" element={<Foro />} /> */}
+        {/* <Route path="/profile" element={<Profile />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/createPokemon" element={<CreatePokemon />} /> */}
+        {/* <Route path="/editPokemon" element={<EditPokemon />} /> */}
+      </Routes>
+    </div>
   );
 }
 
