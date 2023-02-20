9e422d539928e1959888da6d2c891add890b2d03
SantiRosso <santiagorosso@live.com.ar>
2023-02-20 19:01:03 -0300

    app front

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <Routes>
+      {/* <Route exact path="/" element={<LoginRegister />} /> */}
+      {/* <Route path="/home" element={<Home />} /> */}
+      {/* <Route path="/detail/:id" element={<Detail />} /> */}
+      {/* <Route path="/profile" element={<Profile />} /> */}
+      {/* <Route path="/foro" element={<Foro />} /> */}
+      {/* <Route path="/profile" element={<Profile />} /> */}
+      {/* <Route path="/about" element={<About />} /> */}
+      {/* <Route path="/createPokemon" element={<CreatePokemon />} /> */}
+      {/* <Route path="/editPokemon" element={<EditPokemon />} /> */}
+    </Routes>
   );
 }
 
