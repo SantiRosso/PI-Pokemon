4b2ecfb2d12ee5aec119f21eaa90513d331d4190
SantiRosso <santiagorosso@live.com.ar>
2023-02-23 21:14:57 -0300

    new model (User)

---
+++ api/src/models/User.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "user",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      sex: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      favType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: true }
+  );
+};
