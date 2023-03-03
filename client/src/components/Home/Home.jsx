61d75d3bcaa18f693f8feaa73c5ea7a06348f27f
SantiRosso <santiagorosso@live.com.ar>
2023-03-03 14:19:41 -0300

    refactor

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,34 +1,23 @@
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 //components
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import CreatePokemonModal from "../Modals/CreatePokemonModal"
 
 const Home = () => {
+    const navigate = useNavigate()
     return(
         <div>
             <NavBar/>
-            <h1>Home</h1>
-            <Link to="/pokemons">
-                <h1>Pokemons</h1>
-            </Link>
-            <Link to="/profile">
-                <h1>Profile</h1>
-            </Link>
-            <Link to="/foro">
-                <h1>Foro</h1>
-            </Link>
-            <Link to="/about">
-                <h1>About</h1>
-            </Link>
-            {/* <Link to="/create-pokemon">
-                <h1>Create Pokemon</h1>
-            </Link> */}
-            <CreatePokemonModal/>
-            <Link to="/">
-                <h1>Login/Register</h1>
-            </Link>
-            <h1>Logout</h1>
+                Home
+                <button onClick={()=> navigate("/pokemons")}>Pokemons</button>
+                <button onClick={navigate("/profile")}>Profile</button>
+                <button onClick={()=> navigate("/foro")}>Foro</button>
+                <button onClick={()=> navigate("/about")}>About</button>
+                <button onClick={()=> navigate("/create-pokemon")}>Create Pokemon</button>
+                <CreatePokemonModal/>
+                <button onClick={()=> navigate("/")}>Login/Register</button>
+                <button>Logout</button>
             <Footer/>
         </div>
     )
