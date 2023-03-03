8429b27bd9720fcd5a74272bf589b458042ea286
SantiRosso <santiagorosso@live.com.ar>
2023-03-03 15:12:44 -0300

    NavBar styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+//images
+import pokedex from "./pokedex.png"
 //styles
 import s from "./NavBar.module.css";
 //components
@@ -8,7 +10,8 @@ const NavBar = () => {
     return(
         <div className={s.navBar}>
             <Link to="/home">
-                <h1>Pokedex</h1>
+                {/* <h1>Pokedex</h1> */}
+                <img src={pokedex} alt="pokedex" className={s.image}/>
             </Link>
             <SearchBar/>
             <h1>Desplegable</h1>
