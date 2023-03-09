453fa7b4b8ce07ba087a388672130d47cdbfec94
SantiRosso <santiagorosso@live.com.ar>
2023-03-09 02:48:24 -0300

    Deploy

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,7 +7,7 @@ export const RESET_FILTERS = "RESET_FILTERS";
 
 export const getAllPokemons = () => {
   return async (dispatch) => {
-    let result = await axios.get("http://localhost:3001/pokemons");
+    let result = await axios.get("/pokemons");
     return dispatch({ type: GET_ALL_POKEMONS, payload: result.data });
   };
 };
