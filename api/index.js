df5000b5e41ad858f7db92651b5044de7414a04c
SantiRosso <santiagorosso@live.com.ar>
2023-03-09 02:09:36 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -20,11 +20,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
-const { DB_PORT } = process.env || "http://localhost:3001";
+const port = process.env.PORT || "http://localhost:3001";
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
