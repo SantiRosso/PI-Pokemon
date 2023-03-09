8578b89200b12fe560be6c8b7986b103c9b0b2df
SantiRosso <santiagorosso@live.com.ar>
2023-03-09 02:54:41 -0300

    deploy

--- client/src/components/Pokemons/Pokemons.jsx
+++ client/src/components/Pokemons/Pokemons.jsx
@@ -47,7 +47,7 @@ const Pokemons = () => {
             <SearchBar/>
             <div className={s.pokemonsContainer}>
                 {
-                    pokemons?.map((e, i) => {
+                    pokemons.length && pokemons.map((e, i) => {
                         return(
                             <Card key={i} id={e.id} name={e.name} types={e.types} image={e.image?.front_default || "https://w7.pngwing.com/pngs/620/521/png-transparent-poke-ball-pokemon-pokemon-rim-mobile-phones-pokemon.png"}/>
                         )
