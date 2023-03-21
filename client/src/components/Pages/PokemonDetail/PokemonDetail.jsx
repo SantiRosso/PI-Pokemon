935252d9468bbad4a362f86786fb25c142b50e57
SantiRosso <santiagorosso@live.com.ar>
2023-03-20 23:14:33 -0300

    PokemonDetail styles refactor

--- client/src/components/Pages/PokemonDetail/PokemonDetail.jsx
+++ client/src/components/Pages/PokemonDetail/PokemonDetail.jsx
@@ -37,16 +37,18 @@ const PokemonDetail = () => {
                 <h1>{detail?.name}</h1>
                 <div className={s.info}>
                     <img src={detail?.image.front_default} alt={detail?.name} />
-                    <div>
-                        <h1>TYPES: {detail?.types}</h1>
-                        <h1>HP: {detail?.hp}</h1>
-                    </div>
-                    <div>
-                        <h1>ATTACK: {detail?.attack}</h1>
-                        <h1>DEFENSE: {detail?.defense}</h1>
-                        <h1>SPEED: {detail?.speed}</h1>
-                        <h1>HEIGHT: {detail?.height}</h1>
-                        <h1>WEIGHT:{detail?.weight}</h1>
+                    <div className={s.grid}>
+                        <div>
+                            <h1>TYPES: {detail?.types}</h1>
+                            <h1>HP: {detail?.hp}</h1>
+                        </div>
+                        <div>
+                            <h1>ATTACK: {detail?.attack}</h1>
+                            <h1>DEFENSE: {detail?.defense}</h1>
+                            <h1>SPEED: {detail?.speed}</h1>
+                            <h1>HEIGHT: {detail?.height}</h1>
+                            <h1>WEIGHT:{detail?.weight}</h1>
+                        </div>
                     </div>
                 </div> 
             </div>
