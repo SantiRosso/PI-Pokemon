b99bdf543386eda134a29ee5e36a5ef8dbbaf9bf
SantiRosso <santiagorosso@live.com.ar>
2023-03-23 12:38:28 -0300

    new route (createComment)

--- api/src/routes/comments/comments.routes.js
+++ api/src/routes/comments/comments.routes.js
@@ -1,6 +1,10 @@
 const { Router } = require("express");
 const router = Router();
-const { getComments, deleteComment } = require("./controllers.js");
+const {
+  getComments,
+  deleteComment,
+  createComment,
+} = require("./controllers.js");
 
 router.get("/", async (req, res) => {
   try {
@@ -14,6 +18,16 @@ router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.body;
     await deleteComment(id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { text, userId } = req.body;
+    await createComment(text, userId);
     res.sendStatus(204);
   } catch (error) {
     res.status(500).send(error.message);
