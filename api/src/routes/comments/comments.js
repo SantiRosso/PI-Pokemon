eaf29bfe9586e532d7597bae597253d3e368b803
SantiRosso <santiagorosso@live.com.ar>
2023-03-23 11:11:07 -0300

    new route (getComments)

---
+++ api/src/routes/comments/comments.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const router = Router();
+const { getComments } = require("./controllers.js");
+
+router.get("/", async (req, res) => {
+  try {
+    res.send(await getComments());
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
