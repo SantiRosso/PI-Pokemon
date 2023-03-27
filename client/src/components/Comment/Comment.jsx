d5a72e15d2cc6018b5ec316464e7d82b66706f0d
SantiRosso <santiagorosso@live.com.ar>
2023-03-27 16:02:51 -0300

    Comment

---
+++ client/src/components/Comment/Comment.jsx
@@ -0,0 +1,15 @@
+import s from "./Comment.module.css"
+
+const Comment = () => {
+    return(
+        <div>
+            <h4>user name</h4>
+            <h6>text</h6>
+            <div>
+                <h6>ver respuestas...</h6>
+            </div>
+        </div>
+    )
+}
+
+export default Comment;
