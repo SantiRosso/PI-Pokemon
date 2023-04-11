e048a8376d1b2f2d3364e7aa0ffc32c8ca49c198
SantiRosso <santiagorosso@live.com.ar>
2023-04-10 23:30:01 -0300

    feat: lógica del componente Comment

--- client/src/components/Comment/Comment.jsx
+++ client/src/components/Comment/Comment.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react"
 import s from "./Comment.module.css"
 
-const Comment = () => {
+
+const Comment = ({user, text, responses}) => {
+    
+    const [res, setRes] = useState(false)
+
+    const handleClick = () => {
+        setRes(!res)
+    }
+    
     return(
         <div>
-            <h4>user name</h4>
-            <h6>text</h6>
+            <h4>{user}</h4>
+            <h6>{text}</h6>
             <div>
-                <h6>ver respuestas...</h6>
+                <button onClick={handleClick} hidden={!responses && res === false}>ver respuestas...</button>
+            </div>
+            <div>
+                <button onClick={handleClick} hidden={!responses && res === true}>ver respuestas...</button>
+            </div>
+            <div hidden={res}>
+                {
+                    responses?.map((e) => {
+                        return(
+                            <div>
+                                <h1>{responses.user}</h1>
+                                <h4>{responses.text}</h4>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
