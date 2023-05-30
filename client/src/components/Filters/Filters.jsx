954652581fd488c1b2b06079e8bb4ce01b834f49
SantiRosso <santiagorosso@live.com.ar>
2023-05-30 19:03:12 -0300

    lógica de Filters.jsx

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,14 +1,46 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTypes, getFilters } from "../../redux/actions.js"
+
 const Filters = () => {
+
+    const types = useSelector((state) => state.types);
+    const dispatch = useDispatch()
+
+    const [filters, setFilters] = useState({
+        types: ""
+    });
+
+    useEffect(()=> {
+        if(!types) dispatch(getTypes())
+        dispatch(getFilters(filters))
+    })
+
+    const handleChange = (e) => {
+        setFilters({
+            [e.target.name]: e.target.value,
+        })
+    }
+
     return(
         <div>
             <h4>Filters</h4>
             <p>Types</p>
-            <select name="" id=""></select>
+            <select name="types" id="1" onClick={handleChange}>
+                <option value="TYPES">TYPES</option>
+                {
+                    types?.map((type, i) => {
+                        return(
+                            <option key={i} value={type.name}>{type.name}</option>
+                        )
+                    })
+                }
+            </select>
             <h4>Order by:</h4>
             <p>Alphabet</p>
-            <select name="" id=""></select>
+            <select name="" id="2"></select>
             <p>Attack</p>
-            <select name="" id=""></select>
+            <select name="" id="3"></select>
         </div>
     )
 }
