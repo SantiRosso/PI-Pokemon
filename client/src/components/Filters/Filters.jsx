76fbc4f81a7d907555ca2c7553efdef40f2d1ab4
SantiRosso <santiagorosso@live.com.ar>
2023-05-30 01:19:51 -0300

    Filters

---
+++ client/src/components/Filters/Filters.jsx
@@ -0,0 +1,16 @@
+const Filters = () => {
+    return(
+        <div>
+            <h4>Filters</h4>
+            <p>Types</p>
+            <select name="" id=""></select>
+            <h4>Order by:</h4>
+            <p>Alphabet</p>
+            <select name="" id=""></select>
+            <p>Attack</p>
+            <select name="" id=""></select>
+        </div>
+    )
+}
+
+export default Filters;
