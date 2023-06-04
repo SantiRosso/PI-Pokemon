50273ddb02f778778385363ea7063dab6f3cb324
SantiRosso <santiagorosso@live.com.ar>
2023-06-04 14:58:26 -0300

    Pagination styles

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -31,10 +31,10 @@ const Pagination = ({input, setInput, page, setPage, max}) => {
     return(
         <div>
             <div className={s.container}>
-                <button disabled={page === 1} onClick={previousPage}>{"<"}</button>
-                <input onChange={(e) => handleChange(e)} onKeyDown={(e)=> onKeyDown(e)} type="text" name="page" autoComplete="off" value={input}/>
-                <p>of {max}</p>
-                <button disabled={page === max} onClick={nextPage}>{">"}</button>
+                <button className={s.button} disabled={page === 1} onClick={previousPage}>{"<"}</button>
+                <input className={s.input} onChange={(e) => handleChange(e)} onKeyDown={(e)=> onKeyDown(e)} type="text" name="page" autoComplete="off" value={input}/>
+                <p className={s.p}>of {max}</p>
+                <button className={s.button} disabled={page === max} onClick={nextPage}>{">"}</button>
             </div>
         </div>
     )
