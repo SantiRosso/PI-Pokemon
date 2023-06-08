c3abf1294e58c5b30040a77d24f719b42cfc8c6d
SantiRosso <santiagorosso@live.com.ar>
2023-06-07 21:32:35 -0300

    CreatePokemon styles

--- client/src/components/Modals/CreatePokemonModal.jsx
+++ client/src/components/Modals/CreatePokemonModal.jsx
@@ -4,6 +4,8 @@ import { getTypes } from "../../redux/actions";
 //components
 import { useEffect, useState } from "react";
 import Modal from "./Modal"
+//styles
+import s from "./CreatePokemon.module.css";
 
 const CreatePokemonModal = ({isOpen, closeModal}) => {
 
@@ -51,26 +53,27 @@ const CreatePokemonModal = ({isOpen, closeModal}) => {
         <div>
             <Modal isOpen={isOpen} closeModal={closeModal}>
                 <div className="modal-content">
-                    <h1>Create Pokemon</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="">Name:</label>
-                        <input type="text" name="name" onChange={handleChange}/>
-                        <label htmlFor="">HP</label>
-                        <input type="text" name="hp" onChange={handleChange}/>
-                        <label htmlFor="">Attack</label>
-                        <input type="text" name="attack" onChange={handleChange}/>
-                        <label htmlFor="">Defense</label>
-                        <input type="text" name="defense" onChange={handleChange}/>
-                        <label htmlFor="">Speed</label>
-                        <input type="text" name="speed" onChange={handleChange}/>
-                        <label htmlFor="">Height</label>
-                        <input type="text" name="height" onChange={handleChange}/>
-                        <label htmlFor="">Weight</label>
-                        <input type="text" name="witght" onChange={handleChange}/>
-                        <label htmlFor="">imgUrl</label>
-                        <input type="text" name="imgurl" onChange={handleChange}/>
-                        <label htmlFor="">Types</label>
-                        <select name="types" onChange={handleChange}>
+                    {/* <div className={s.container}> */}
+                        <h1>Create Pokemon</h1>
+                    <form className={s.form} onSubmit={handleSubmit}>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" id="name" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="hp">HP</label>
+                        <input type="text" name="hp" id="hp" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="attack" >Attack</label>
+                        <input type="text" name="attack" id="attack" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="defense">Defense</label>
+                        <input type="text" name="defense" id="defense" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="speed">Speed</label>
+                        <input type="text" name="speed" id="speed" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="height">Height</label>
+                        <input type="text" name="height" id="height" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="weight">Weight</label>
+                        <input type="text" name="witght" id="weight" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="imgurl">imgUrl</label>
+                        <input type="text" name="imgurl" id="imgurl" className={s.input} onChange={handleChange}/>
+                        <label htmlFor="types">Types</label>
+                        <select name="types" id="types" onChange={handleChange}>
                             <option value="types" id="types">TYPES</option>
                             {
                                 types?.map((e) => {
@@ -82,6 +85,7 @@ const CreatePokemonModal = ({isOpen, closeModal}) => {
                         </select>
                         <button type="submit">Crear</button>
                     </form>
+                    {/* </div> */}
                 </div>
             </Modal>
         </div>
